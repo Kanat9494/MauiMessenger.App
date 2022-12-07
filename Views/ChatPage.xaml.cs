df5204a1816711ec550b516a7471fe857b4cffab
kana2022 <kkanat94@gmail.com>
2022-12-07 18:31:15 +0600

    added new pages

---
+++ Views/ChatPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiMessenger.Views;
+
+public partial class ChatPage : ContentPage
+{
+	public ChatPage()
+	{
+		InitializeComponent();
+		this.BindingContext = new ChatViewModel();
+	}
+
+	private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
+	{
+		(this.BindingContext as ChatViewModel).Initialize();
+	}
+}
