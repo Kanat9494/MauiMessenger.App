0aca52b7c36eb5c7b797feba93f980bab9d86e0a
kana2022 <kkanat94@gmail.com>
2022-12-13 17:25:12 +0600

    added new model

---
+++ Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiMessenger.Models;
+
+public class Message
+{
+    public int MessageId { get; set; } 
+    public int FromUserId { get; set; }
+    public int ToUserId { get; set; }   
+    public string Content { get; set; }
+    public DateTime SentDateTime { get; set; }
+    public bool IsRead { get; set; }
+}
